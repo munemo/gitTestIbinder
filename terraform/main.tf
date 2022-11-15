806e9b9c43808a9536d3f17391aea702abf87183
John Chimbani <35688814+munemo@users.noreply.github.com>
2022-11-15 13:45:50 +0100

    removing fmt script13

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,26 @@
 terraform {
   backend "azurerm" {
-     resource_group_name  = "ibinder-resource-group3"
+    resource_group_name  = "ibinder-resource-group3"
     storage_account_name = "ibinderstorage"
     container_name       = "tfstate"
     key                  = "terraformgithubexample.tfstate"
   }
 }
- 
+
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
   #version = "~>2.0"
   #features {}
 }
- 
+
 data "azurerm_client_config" "current" {}
- 
-resource "azurerm_resource_group" "rg"{
+
+resource "azurerm_resource_group" "rg" {
   name     = "terraformIbinder"
   location = "northeurope"
-    tags = {
-    Owner = "john.chimbani"
+  tags = {
+    Owner   = "john.chimbani"
     DueDate = "2022-11-02"
   }
 }
