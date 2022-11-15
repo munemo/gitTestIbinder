fa68f670c72263b749c1ca6a36e8d56551090cb5
John Chimbani <35688814+munemo@users.noreply.github.com>
2022-11-15 11:48:54 +0100

    updating main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,27 @@
 terraform {
- required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-
-   backend "azurerm" {
-    resource_group_name  = "ibinder-resource-group3"
+  backend "azurerm" {
+     resource_group_name  = "ibinder-resource-group3"
     storage_account_name = "ibinderstorage"
     container_name       = "tfstate"
     key                  = "terraformgithubexample.tfstate"
   }
 }
-
+ 
 provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
   features {}
 }
-
+ 
 data "azurerm_client_config" "current" {}
-
-
+ 
 resource "azurerm_resource_group" "rg"{
-  name     = "terraformIbinderResourceGroup"
+  name     = "terraformIbinder"
   location = "northeurope"
     tags = {
     Owner = "john.chimbani"
     DueDate = "2022-11-02"
   }
 }
-
-
-
-
+ 
