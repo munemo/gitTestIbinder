30619ece871d14c6239fd0acfaa9443b43c1df5b
munemo <john.chimbani@outlook.com>
2022-11-16 10:46:47 +0100

    merging previous

--- terraform/key_vault/main.tf
+++ terraform/key_vault/main.tf
@@ -5,10 +5,10 @@ terraform {
       version = "=3.0.0"
     }
   }
-backend "azurerm" {
-        resource_group_name  = "ibinder-resource-group3"
-        storage_account_name = "ibinderstorage"
-        container_name       = "tfstate2"
-        key                  = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "ibinder-resource-group3"
+    storage_account_name = "ibinderstorage"
+    container_name       = "tfstate2"
+    key                  = "terraform.tfstate"
+  }
 }
