ebae946e38d7af578c8b33b9915d201fe1c90fe9
munemo <john.chimbani@outlook.com>
2022-11-16 11:01:50 +0100

    merging previous

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,32 +46,3 @@ resource "azurerm_key_vault" "keyvault" {
     ]
   }
 }
-/*terraform {
-  backend "azurerm" {
-    resource_group_name  = "ibinder-resource-group3"
-    storage_account_name = "ibinderstorage"
-    container_name       = "tfstate"
-    key                  = "ibinder.tfstate"
-    subscription_id      = "ae6cbacb-2eac-42cc-978e-516b8ef7628d"
-    tenant_id            = "7bb1a8e5-59ee-489d-86f5-a50210ae3970"
-    access_key           = "pBvukWdGOxj+l2XCYCmda4v8Uc44VBYupG1y6CWOmifrCqcPhuNOd3vJEAmi/okMdVa+D5BGbvr/+ASt9nDNgQ=="
-  }
-}
-
-provider "azurerm" {
-  skip_provider_registration = "true"
-  features {}
-  use_msi = true
-}
-
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "terraformIbinder"
-  location = "northeurope"
-  tags = {
-    Owner   = "john.chimbani"
-    DueDate = "2022-11-02"
-  }
-}*/
- 
